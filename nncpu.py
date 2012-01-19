fb4332512ca9f8a8b6f13725b383d6d97f41d514
David Stalnaker <david.stalnaker@gmail.com>
2012-01-18 23:15:07 -0500

    rewrote to have a variable number of hidden nodes and +100 code readability

--- nncpu.py
+++ nncpu.py
@@ -1,9 +1,8 @@
-import sys
 import math
 from numpy import mat, concatenate, vectorize, ones, multiply
 from numpy.random import rand
 
-training = [([1,1], [0]), ([1,-1], [1]), ([-1,1], [1]), ([-1,-1], [0])]
+training = [([1,1], [0]), ([1,0], [1]), ([0,1], [1]), ([0,0], [0])]
 train2 = [
     ([0.10,0.03], [1, 0]),
     ([0.15,0.13], [1, 0]),
@@ -29,73 +28,61 @@ def pad(x):
 
 class NeuralNet(object):
     def __init__(self, structure, eta=0.1):
-        self.input_nodes = structure[0]
-        self.hidden_nodes = structure[1]
-        self.output_nodes = structure[2]
+        self.structure = list(structure)
         self.eta = eta
         self.reset_weights()
 
     def reset_weights(self):
-        self.hidden_weights = mat(rand(self.input_nodes + 1, self.hidden_nodes)) * 2 - 1
-        self.output_weights = mat(rand(self.hidden_nodes + 1, self.output_nodes)) * 2 - 1
-
-        #print(self.hidden_weights)
-        #print(self.output_weights)
+        self.weights = []
+        for i in range(len(self.structure) - 1):
+            inp = self.structure[i]
+            out = self.structure[i + 1]
+            self.weights.append(mat(rand(inp + 1, out)) * 2 - 1)
 
     def train(self, samples, epochs=1000):
-        w_1 = self.hidden_weights
-        w_2 = self.output_weights
-
         for i in range(epochs):
             for s in samples:
-                x = mat(s[0]).T
-                y = mat(s[1]).T
-                a, b = self.run(x.T, verbose=True)
-                a_1 = a[0]
-                a_2 = a[1]
-                b_1 = b[0]
-                b_2 = b[1]
+                input = mat(s[0]).T
+                truth = mat(s[1]).T
 
-                d_2 = y - b_2
-                print('error = %f' % d_2[0][0])
-                d_1 = (d_2.T * w_2.T).T
+                sums, outputs = self.run(input.T, verbose=True)
+                inputs = [input]
+                inputs.extend(outputs[:-1])
+                errors = []
 
-                e_1 = self.eta * pad(x) * multiply(d_1[1:].T, dsigmoid(a_1.T))
-                self.hidden_weights = self.hidden_weights + e_1
+                # calculate errors at each layer
+                error = truth - outputs[-1]
+                errors.insert(0, error)
+                for weights in reversed(self.weights[1:]):
+                    error = (errors[0].T * weights.T).T[1:]
+                    errors.insert(0, error)
 
-                e_2 = self.eta * b_1 * multiply(d_2.T, dsigmoid(a_2.T))
-                self.output_weights = self.output_weights + e_2
+                # update each set of weights
+                for i, weights in enumerate(self.weights):
+                    input = inputs[i]
+                    sum = sums[i]
+                    error = errors[i]
 
+                    adjustments = self.eta * pad(input) * multiply(error.T, dsigmoid(sum.T))
+                    self.weights[i] = self.weights[i] + adjustments
 
-#        except ValueError as e:
-#            print(e.message)
-#            tb = sys.exc_traceback
-#            while tb.tb_next:
-#                tb = tb.tb_next
-#            args = tb.tb_frame.f_locals
-#            print('first:')
-#            print(args['self'])
-#            print('second:')
-#            print(args['other'])
-
-
-
+                print(error)
 
     def run(self, input, verbose=False):
-        w_1 = self.hidden_weights
-        w_2 = self.output_weights
-        x = pad(mat(input).T)
+        output = mat(input).T
+        sums = []
+        outputs = []
 
-        a_1 = (x.T * w_1).T
-        b_1 = pad(sigmoid(a_1))
-
-        a_2 = (b_1.T * w_2).T
-        b_2 = sigmoid(a_2)
+        for weights in self.weights:
+            sum = (pad(output).T * weights).T
+            output = sigmoid(sum)
+            sums.append(sum)
+            outputs.append(output)
 
         if verbose:
-            return [a_1,a_2], [b_1,b_2]
+            return sums, outputs
         else:
-            return b_2
+            return output
 
 
 if __name__ == '__main__':
