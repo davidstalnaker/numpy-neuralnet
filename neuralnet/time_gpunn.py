153258c6c46a0ded1258ab05f8283ac750232955
Max Bogue <max@maxbogue.com>
2012-02-17 18:12:10 -0500

    Possibly finished the time_gpunn script.  Getting some weird results.

--- neuralnet/time_gpunn.py
+++ neuralnet/time_gpunn.py
@@ -2,23 +2,27 @@ from neuralnet import *
 
 def time_gpunn(name, filename, nn_structure, **kwargs):
     train, val, test = read_samples(filename, **kwargs)
+
     cpunn = NeuralNet(nn_structure)
-
-    print '%s running sequential training.' % name
-    t1 = timef(cpunn.train, train, val, epochs=100)
-
-    print '%s running sequential test.' % name
-    t2 = timef(cpunn.test, test)
     gpunn = GpuNeuralNet(nn_structure)
 
-    print '%s running sequential training.' % name
-    t3 = timef(gpunn.train, train, val, epochs=100)
+    print '%s: running CPU feed-forward.' % name
+    t1 = timef(cpunn.test, test)
+    print '%s: loading GPU feed-forward buffers.' % name
+    t2 = timef(gpunn.init_feed_forward_buffers, test)
+    print '%s: running GPU feed-forward.' % name
+    t3 = timef(gpunn.feed_forward)
 
-    print '%s running sequential test.' % name
-    t4 = timef(gpunn.test, test)
-    return t1, t2, t3, t4
+    print '%s: running CPU back-propagation.' % name
+    t4 = timef(run_backprop, cpunn, train, 10000)
+    print '%s: loading GPU back-propagation buffers.' % name
+    t5 = timef(gpunn.init_backprop_buffers, train)
+    print '%s: running GPU back-propagation.' % name
+    t6 = timef(gpunn.gpu_backprop, 10000)
 
-def run_backprop(count, net, samples):
+    return t1, t2, t3, t4, t5, t6
+
+def run_backprop(net, samples, count):
     for i in range(count):
         net.backprop(samples[i % len(samples)])
 
